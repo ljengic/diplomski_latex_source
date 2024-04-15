1a1672910b8250f880eea1054821d82871a1d257
Luka Jengic <luka.jengic@gmail.com>
2024-04-15 10:38:58 -0400

    add script for automatic compile

---
+++ compile.bat
@@ -0,0 +1,14 @@
+::delete all build files
+del diplomski_rad.aux
+del diplomski_rad.bbl
+del diplomski_rad.blg
+del diplomski_rad.log
+del diplomski_rad.out
+del diplomski_rad.pdf
+del diplomski_rad.toc
+
+:: build .pdf
+pdflatex diplomski_rad.tex
+bibtex diplomski_rad
+pdflatex diplomski_rad.tex
+pdflatex diplomski_rad.tex
